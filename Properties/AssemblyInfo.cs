3545839c3506531a7f4388e544c5aa91d37e600d
CatYoutuber <cat158196@gmail.com>
2022-12-27 02:41:57 +0200

    Update info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанных со сборкой.
 [assembly: AssemblyTitle("XMasLights")]
-[assembly: AssemblyDescription("XMas Lights")]
+[assembly: AssemblyDescription("The Christmas Lights Project")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CatYoutuber TV")]
 [assembly: AssemblyProduct("XMasLights")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
